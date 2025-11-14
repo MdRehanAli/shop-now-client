c94b4244222072de9fd7814f15c043a0ebb393f7
MdRehanAli <rehanali21d@gmail.com>
2025-11-14 21:36:40 +0600

    Added private route

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -2,13 +2,13 @@ import React, { use } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { Navigate, useLocation } from 'react-router';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user } =use(AuthContext);
+    const { user } = use(AuthContext);
 
     const location = useLocation()
 
-    if(user){
+    if (user) {
         return children
     }
 
